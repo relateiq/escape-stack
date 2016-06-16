30cabe5847ed3e766e7be1b8151b404cf4c40371
Varun Munjeti <vrunjeti@gmail.com>
2016-06-16 10:56:28 -0700

    abstracted add and pop in tests

--- src/test.spec.ts
+++ src/test.spec.ts
@@ -7,6 +7,18 @@ import escapeStack from './';
 
 const NUM_TIMES = 5;
 
+function addToStack(escapeStack, t, numTimes) {
+	for (let i = 0; i < numTimes; i++) {
+		escapeStack.add(() => t.pass());
+	}
+}
+
+function popOffStack(escapeStack, numTimes) {
+	for (let i = 0; i < numTimes; i++) {
+		escapeStack.pop();
+	}
+}
+
 test('it should call the handler\'s callback when popped', t => {
 	t.plan(1);
 	const es = escapeStack();
@@ -17,53 +29,35 @@ test('it should call the handler\'s callback when popped', t => {
 test('it should call every handler\'s callback when popped', t => {
 	t.plan(NUM_TIMES);
 	const es = escapeStack();
-
-	for (let i = 0; i < NUM_TIMES; i++) {
-		es.add(() => t.pass());
-	}
-	for (let i = 0; i < NUM_TIMES; i++) {
-		es.pop();
-	}
+	addToStack(es, t, NUM_TIMES);
+	popOffStack(es, NUM_TIMES);
 });
 
 test('it should not call any callbacks when popping an empty stack', t => {
 	t.plan(NUM_TIMES);
 	const es = escapeStack();
-
-	for (let i = 0; i < NUM_TIMES; i++) {
-		es.add(() => t.pass());
-	}
-	for (let i = 0; i < NUM_TIMES; i++) {
-		es.pop();
-	}
+	addToStack(es, t, NUM_TIMES);
+	popOffStack(es, NUM_TIMES);
 
 	// stack should be empty now, popping should not have any effect
-	for (let i = 0; i < NUM_TIMES; i++) {
-		es.pop();
-	}
+	popOffStack(es, NUM_TIMES);
 });
 
 test('it should remove handlers in the stack without calling their callbacks', t => {
 	t.plan(NUM_TIMES * 2);
 	const es = escapeStack();
 
-	for (let i = 0; i < NUM_TIMES; i++) {
-		es.add(() => t.pass());
-	}
+	addToStack(es, t, NUM_TIMES);
 
 	// add a handler that will be removed into the middle of the stack
 	const removeHandler = es.add(() => t.pass());
 
-	for (let i = 0; i < NUM_TIMES; i++) {
-		es.add(() => t.pass());
-	}
+	addToStack(es, t, NUM_TIMES);
 
 	// this should not call its callback
 	removeHandler();
 
 	// despite pop being called the total number of handlers times,
 	// it should not execute the handlers that were removed
-	for (let i = 0; i < (NUM_TIMES * 2) + 1; i++) {
-		es.pop();
-	}
+	popOffStack(es, NUM_TIMES + 1);
 });
