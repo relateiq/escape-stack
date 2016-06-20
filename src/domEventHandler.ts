97237b1f61e85a9cfc2cd1112eeac98b7a45396a
Varun Munjeti <vrunjeti@gmail.com>
2016-06-20 11:56:09 -0700

    fixed exports by adding globalStack option

--- src/domEventHandler.ts
+++ src/domEventHandler.ts
@@ -1,6 +1,5 @@
 const ESCAPE_KEY_CODE = 27;
 
-// todo: change typing to be function that takes one param of type event?
 export function handleEscapeKeydown(element: Node, callback: Function) {
 	element.addEventListener('keydown', function (event: KeyboardEvent) {
 		const key = event.keyCode || event.which;
--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,23 @@
 import { EscapeStack } from './EscapeStack';
 import { handleEscapeKeydown } from './domEventHandler';
 
+let globalStack;
+
 const es = new EscapeStack();
 handleEscapeKeydown(document.body, es.pop);
 
-export default es;
+function createEscapeStack(global: boolean = false) {
+    const es = new EscapeStack();
+    handleEscapeKeydown(document.body, es.pop);
+
+    if (!global) {
+        return es;
+    }
+
+    if (!globalStack) {
+        globalStack = es;
+    }
+    return globalStack;
+}
+
+export default createEscapeStack;
