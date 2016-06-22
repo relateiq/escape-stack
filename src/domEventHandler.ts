04af167b7ff8d79a0f910146f5d199a03f56a4c6
Varun Munjeti <shadow.vrunjeti@gmail.com>
2016-06-22 15:33:55 -0700

    converted from 4 spaces to 2

--- src/domEventHandler.ts
+++ src/domEventHandler.ts
@@ -1,10 +1,10 @@
 const ESCAPE_KEY_CODE = 27;
 
 export function handleEscapeKeydown(element: Node, callback: Function) {
-    element.addEventListener('keydown', function (event: KeyboardEvent) {
-        const key = event.keyCode || event.which;
-        if (key === ESCAPE_KEY_CODE) {
-            callback(event);
-        }
-    })
+  element.addEventListener('keydown', function (event: KeyboardEvent) {
+    const key = event.keyCode || event.which;
+    if (key === ESCAPE_KEY_CODE) {
+      callback(event);
+    }
+  });
 }
--- src/index.ts
+++ src/index.ts
@@ -5,20 +5,20 @@ import { handleEscapeKeydown } from './domEventHandler';
 let globalStack;
 
 function initStack() {
-    const escapeStack = new EscapeStack();
-    handleEscapeKeydown(document.body, escapeStack.pop);
-    return escapeStack;
+  const escapeStack = new EscapeStack();
+  handleEscapeKeydown(document.body, escapeStack.pop);
+  return escapeStack;
 }
 
 function createEscapeStack(global: boolean = false) {
-    if (!global) {
-        return initStack();
-    }
+  if (!global) {
+    return initStack();
+  }
 
-    if (!globalStack) {
-        globalStack = initStack();
-    }
-    return globalStack;
+  if (!globalStack) {
+    globalStack = initStack();
+  }
+  return globalStack;
 }
 
 export default createEscapeStack;
