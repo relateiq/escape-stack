1d35141fdd65b53d7a8d5eb8d8829e9e5a8e1c31
Varun Munjeti <vrunjeti@gmail.com>
2016-06-27 17:24:53 -0700

    in progress - demo page: added boxes and fancy boxes

--- app/index.ts
+++ app/index.ts
@@ -1,2 +1,29 @@
 ///<reference path="../typings/index.d.ts" />
-import app from '../.src';
+import createEscapeStack from '../.src';
+
+const escapeStack = createEscapeStack();
+console.log(escapeStack);
+
+const addBoxButton = document.getElementById('js-add-box');
+const addFancyBoxButton = document.getElementById('js-add-fancy-box');
+
+addBoxButton.addEventListener('click', event => {
+  addBox();
+});
+
+addFancyBoxButton.addEventListener('click', event => {
+  addBox(true);
+});
+
+function addBox(fancy: boolean = false) {
+  const boxDiv = `<div class="col-md-4">
+      <div class="demo-box ${fancy ? 'fancy' : ''}"></div>
+    </div>`;
+  const boxContainer = document.getElementById('box-container');
+  const allBoxesDivs = boxContainer.innerHTML;
+  boxContainer.innerHTML = allBoxesDivs + boxDiv;
+}
+
+function deleteBox(div: Element) {
+
+}
