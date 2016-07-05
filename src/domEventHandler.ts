160413b4e560b9136e321b3c3c55966e5acd576d
Varun Munjeti <vrunjeti@gmail.com>
2016-07-05 13:09:39 -0700

    wrapped dom handler in DOMContentReady

--- src/domEventHandler.ts
+++ src/domEventHandler.ts
@@ -1,10 +1,12 @@
 const ESCAPE_KEY_CODE = 27;
 
 export function handleEscapeKeydown(element: Node, callback: Function) {
-  element.addEventListener('keydown', function (event: KeyboardEvent) {
-    const key = event.keyCode || event.which;
-    if (key === ESCAPE_KEY_CODE) {
-      callback(event);
-    }
-  }, true);
+  document.addEventListener('DOMContentLoaded', () => {
+    element.addEventListener('keydown', function (event: KeyboardEvent) {
+      const key = event.keyCode || event.which;
+      if (key === ESCAPE_KEY_CODE) {
+        callback(event);
+      }
+    }, true);
+  });
 }
