a66ff528d8abc671c52fddb89ddb4ea7af1a8b78
Varun Munjeti <vrunjeti@gmail.com>
2016-07-05 14:18:07 -0700

    toggle single pop and pop all and added animations to demo

--- app/index.ts
+++ app/index.ts
@@ -1,17 +1,36 @@
 ///<reference path="../typings/index.d.ts" />
 import createEscapeStack from '../.src';
 
+const ANIMATION_TIME = 1000;
+
 const escapeStack = createEscapeStack();
+let singlePopFlag = true;
 
 const addBoxButton = document.getElementById('js-add-box');
 const addFancyBoxButton = document.getElementById('js-add-fancy-box');
+const toggleSinglePopButton = document.getElementById('js-toggle-single-pop');
 
-addBoxButton.addEventListener('click', _ => addBox());
-addFancyBoxButton.addEventListener('click', _ => addBox(true));
+addBoxButton.addEventListener('click', _ => addBox(escapeStack, false));
+addFancyBoxButton.addEventListener('click', _ => addBox(escapeStack, true));
+toggleSinglePopButton.addEventListener('click', event => {
+  toggleSinglePop(event.target);
+});
 
-function onEscape(isSinglePop: boolean = true, event) {
-  this.remove();
-  return isSinglePop;
+function toggleSinglePop(button) {
+  singlePopFlag = !singlePopFlag;
+  const popText = `${singlePopFlag ? 'Single Pop' : 'Pop All'}`;
+  const singlePopButtonText = `Currently set to: ${popText} (Click to toggle)`;
+  button.innerHTML = singlePopButtonText;
+}
+
+function onEscape(event) {
+  const demoBox = this.getElementsByClassName('demo-box')[0];
+  demoBox.classList.remove('loading');
+  demoBox.classList.add('closing');
+  setTimeout(() => {
+    this.remove();
+  }, ANIMATION_TIME);
+  return singlePopFlag;
 }
 
 function onRemoveFancy(removeEscapeHandler: Function, event) {
@@ -21,7 +40,7 @@ function onRemoveFancy(removeEscapeHandler: Function, event) {
   this.remove();
 }
 
-function addBox(isFancy: boolean = false) {
+function addBox(escapeStack, isFancy: boolean = false) {
   let newBoxClasses = 'demo-box';
   if (isFancy) {
     newBoxClasses += ' fancy';
@@ -32,7 +51,9 @@ function addBox(isFancy: boolean = false) {
     removeFancyButton = `<div class="remove-fancy js-remove-fancy">Remove Fancy</div>`;
   }
 
-  const newBoxDiv = `<div class="${newBoxClasses}">
+  const newBoxColorStyle = `background-color: ${getRandomColor()}`;
+
+  const newBoxDiv = `<div class="${newBoxClasses}" style="${newBoxColorStyle}">
       ${removeFancyButton}
     </div>`;
 
@@ -41,13 +62,23 @@ function addBox(isFancy: boolean = false) {
   newBoxContainer.innerHTML = newBoxDiv;
 
   if (isFancy) {
-    // passing `true` to stop the EscapeStack's pop function from popping everything in the stack
-    const removeEscapeHandler = escapeStack.add(onEscape.bind(newBoxContainer, true));
-    const removeFancyButtonElement = newBoxContainer.getElementsByClassName('js-remove-fancy')[0]
+    const removeEscapeHandler = escapeStack.add(onEscape.bind(newBoxContainer));
+    const removeFancyButtonElement = newBoxContainer.getElementsByClassName('js-remove-fancy')[0];
     const boundedRemove = onRemoveFancy.bind(removeFancyButtonElement, removeEscapeHandler);
     removeFancyButtonElement.addEventListener('click', boundedRemove);
   }
 
   const allBoxesContainer = document.getElementById('all-boxes-container');
   allBoxesContainer.appendChild(newBoxContainer);
+
+  newBoxContainer.getElementsByClassName('demo-box')[0].classList.add('loading');
+}
+
+function getRandomColor() {
+  const letters = '0123456789ABCDEF'.split('');
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
 }
