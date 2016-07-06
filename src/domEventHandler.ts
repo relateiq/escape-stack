65c6c415fd22da22bcd7625d95e8b45259a084bb
Varun Munjeti <kmunjeti@salesforce.com>
2016-07-06 15:43:27 -0700

    removed DOMContentReady handler

--- src/domEventHandler.ts
+++ src/domEventHandler.ts
@@ -1,13 +1,10 @@
 const ESCAPE_KEY_CODE = 27;
 
 export function handleEscapeKeydown(element: Node, callback: Function) {
-  document.addEventListener('DOMContentLoaded', event => {
-    document.removeEventListener(event.type, this.handleEscapeKeydown, true);
-    element.addEventListener('keydown', function (event: KeyboardEvent) {
-      const key = event.keyCode || event.which;
-      if (key === ESCAPE_KEY_CODE) {
-        callback(event);
-      }
-    }, true);
+  element.addEventListener('keydown', function (event: KeyboardEvent) {
+    const key = event.keyCode || event.which;
+    if (key === ESCAPE_KEY_CODE) {
+      callback(event);
+    }
   }, true);
 }
