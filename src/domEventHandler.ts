18a3ac21de443d873f1cc76b0ff6f38bc84beef1
Varun Munjeti <kmunjeti@salesforce.com>
2016-07-06 14:12:17 -0700

    remove DOMContentLoaded handler after its callback gets called

--- src/domEventHandler.ts
+++ src/domEventHandler.ts
@@ -1,12 +1,13 @@
 const ESCAPE_KEY_CODE = 27;
 
 export function handleEscapeKeydown(element: Node, callback: Function) {
-  document.addEventListener('DOMContentLoaded', () => {
+  document.addEventListener('DOMContentLoaded', event => {
+    document.removeEventListener(event.type, this.handleEscapeKeydown, true);
     element.addEventListener('keydown', function (event: KeyboardEvent) {
       const key = event.keyCode || event.which;
       if (key === ESCAPE_KEY_CODE) {
         callback(event);
       }
     }, true);
-  });
+  }, true);
 }
